37fc4fcd8ee1a5c9b649212324015796b446fece
Dumitru432 <89398647+Dumitru432@users.noreply.github.com>
2023-03-23 09:23:31 +0200

    promises with await keyword

--- main.js
+++ main.js
@@ -7,10 +7,34 @@ const delaydColorChange = (color, delay) => {
     })
 }
 
-delaydColorChange('red', 1000)
+/* delaydColorChange('red', 1000)
     .then(()=> delaydColorChange('orange', 1000))
     .then(()=> delaydColorChange('yellow', 1000))
     .then(()=> delaydColorChange('green', 1000))
     .then(()=> delaydColorChange('blue', 1000))
     .then(()=> delaydColorChange('indigo', 1000))
     .then(()=> delaydColorChange('violet', 1000))
+
+     */
+
+    // with Await Keyword
+
+    async function rainbow () {
+        await delaydColorChange('red', 1000)
+        await delaydColorChange('orange', 1000)
+        await delaydColorChange('yellow', 1000)
+        await delaydColorChange('green', 1000)
+        await delaydColorChange('blue', 1000)
+        await delaydColorChange('indigo', 1000)
+        await delaydColorChange('violet', 1000)
+        return 'all done'
+    }
+
+//  rainbow().then(()=>console.log('End of rainbow')) 
+
+async function printRainbow () {
+    await rainbow();
+    console.log('END OF RAINBOW!')
+};
+printRainbow();
+
